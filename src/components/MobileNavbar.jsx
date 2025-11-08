2307450e5c39747bd9a6a1c33efd90af972ac3c3
Nihat774 <nihatmemmedov.0520@gmail.com>
2025-11-07 23:06:02 -0800

    changed navbar style

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -9,13 +9,12 @@ function MobileNavbar() {
       <div className='flex md:hidden'>
         <VscListSelection className='text-2xl cursor-pointer' onClick={() => setIsOpen(!isOpen)} />
       </div>
-      {
-        isOpen && (
-          <div className={`${isOpen ? "translate-x-0" : "-translate-x-full"} transform transition-transform duration-300 w-full h-screen absolute right-0 top-0 p-10 bg-black text-white`}>
+     
+          <div className={`${isOpen ? "translate-x-0" : "-translate-x-full"} flex flex-col gap-10 w-[75%] transform transition-transform duration-300  h-screen absolute z-20 left-0 top-0 p-10 bg-black text-white`}>
             <div className='flex justify-end'>
               <VscChromeClose className='text-4xl cursor-pointer' onClick={() => setIsOpen(!isOpen)} />
             </div>
-            <div className='flex flex-col gap-5 h-full justify-center items-center  text-3xl '>
+            <div className='flex flex-col gap-5 justify-center  text-3xl '>
               <a
                 onClick={() => setIsOpen(!isOpen)}
                 href="#"
@@ -32,6 +31,10 @@ function MobileNavbar() {
               }
             </div>
           </div>
+      
+      {
+        isOpen && (
+          <div className='inset-0 h-screen fixed z-10 opacity-40 bg-black' onClick={() => setIsOpen(!isOpen)}></div>
         )
       }
     </>
